ae955cf068dad2611f898f21265ad7c632d9971e
velasquez8l <david595.velasquez@gmail.com>
2022-03-02 18:56:53 -0500

    archovos JS y HTML

---
+++ figuras.js
@@ -0,0 +1,9 @@
+const lc =5;
+console.log("los lados del cuadrado miden: "+lc)
+
+const per_cuad=lc*4;
+console.log("el perimetro del cuadrado es: "+per_cuad)
+
+const area_cuad=lc*lc;
+console.log("el area del cuadrado es: "+area_cuad)
+
