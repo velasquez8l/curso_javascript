51b5e425ece5355c54c17e2cb6ec53669eaf9e7a
velasquez8l <david595.velasquez@gmail.com>
2022-03-05 16:14:05 -0500

    falta todavia mejorar el codigo por que no corre

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -9,9 +9,26 @@ function precios(precio,descuento){
 function des(){
     var precio=document.getElementById("precio").value;
     var descuento=document.getElementById("descuento").value;
+    var cupones =document.getElementById("cupon").value;
 
-    var res = precios(precio,descuento);
-    document.getElementById("resultado").innerHTML="el precio nuevo "+
-    `es de ${res}`;
+    var valor_cupon={"hola":10,"hola1":20,"hola2":50};
 
+    
+    if (typeof descuento==="undefined" && typeof cupones!=="undefined"){
+        for (var i in valor_cupon ){
+            if(cupones===i){
+                var descuento=valor_cupon[cupones];
+            }
+        }
+        
+        var res = precios(precio,descuento);
+        document.getElementById("resultado").innerHTML="el precio nuevo "+
+        `es de ${res}`;
+
+    }else if (typeof descuento!=="undefined" && typeof cupones==="undefined") {
+        var res = precios(precio,descuento);
+        document.getElementById("resultado").innerHTML="el precio nuevo "+
+        `es de ${res}`;
+    }
 }
+
