c68da0f54424d7f8815f1b621b3fff182cd6ec13
velasquez8l <david595.velasquez@gmail.com>
2022-03-07 14:32:20 -0500

    cambios definitivos a la pagina

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -14,21 +14,36 @@ function des(){
     var valor_cupon={"hola":10,"hola1":20,"hola2":50};
 
     
-    if (typeof descuento==="undefined" && typeof cupones!=="undefined"){
+    if (descuento==="" &&  cupones!==""){
         for (var i in valor_cupon ){
             if(cupones===i){
+                console.log( i)
                 var descuento=valor_cupon[cupones];
+                var res = precios(precio,descuento);
+                document.getElementById("resultado").innerHTML="el precio nuevo "+
+                `es de ${res}`;
+                break;
+            }else {
+                console.log( cupones)
+                document.getElementById("resultado").innerHTML=`el cupon no 
+                es valido, ingrese un cupon valido. Usted ingreso el cupon ${cupones}`;
             }
         }
         
+        
+
+    }else if ( descuento!=="" &&  cupones==="") {
         var res = precios(precio,descuento);
         document.getElementById("resultado").innerHTML="el precio nuevo "+
         `es de ${res}`;
 
-    }else if (typeof descuento!=="undefined" && typeof cupones==="undefined") {
-        var res = precios(precio,descuento);
-        document.getElementById("resultado").innerHTML="el precio nuevo "+
-        `es de ${res}`;
+    }else if ( descuento!=="" &&  cupones!==""){
+        document.getElementById("resultado").innerHTML=`no puede ingresar un cupon
+         y un descuento al mismo tiempo, 
+          debe decidir uno de los dos `
+    }else{
+        document.getElementById("resultado").innerHTML=`ingrese valores
+         en los campos, no los deje vacios`
     }
 }
 
